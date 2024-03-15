a4c1c13dd8c82c1d19bb8ff525916473c9439f40
Chandan-DK <chandandk468@gmail.com>
2024-03-15 17:57:50 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -1,9 +1,3 @@
-function addSkillsOfAMember(){
-    var member = {
-        name: 'John',
-        skills: ['JavaScript', 'React', 'Node']
-    }
-    member.skills.push('HTML');
-    member.skills.push('CSS');
-    console.log(member);
+function skillsMember() {
+  return this.skills;
 }
